a8fe17064c49c17909d371f63d8e34212a92208b
Itcorner <info@itcorner.org.in>
2016-08-31 17:07:45 +0530

    css basics

---
+++ css.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Css Demo</title>
+
+	 <!-- internal in-file inline --> 
+	<style type="text/css">
+	</style>
+	<!-- external css -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<span> span1 </span>
+<span class="cls" > span 2</span>
+<div>
+	Div 1
+</div>
+<div class="cls">
+	
+	Div 2
+</div>
+<div > <!-- inline style -->
+	Div 3
+
+	<ul>
+		<li>a </li>
+		<li>b</li>
+		<li>c</li>
+	</ul>
+
+
+</div>
+
+<p class="cls">
+	P 1
+</p>
+
+<p id="p2" >
+	P2
+</p>
+
+<ul>
+	<li>1</li>
+	<li>2</li>
+	<li>3</li>
+</ul>
+
+
+
+</body>
+</html>
